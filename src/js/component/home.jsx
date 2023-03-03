1ad128ea31983dcbe921e64a9e875ab4186bc42c
Sara Enríquez <sara_enriquezdesalamanca@hotmail.com>
2023-03-03 19:01:47 +0000

    add buttons

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,15 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div>
-			
-		</div>
-	);
+  const [glow, setGlow] = useState({
+    red: "",
+    yellow: "",
+    green: "",
+  });
+
+  useEffect(() => {
+    let glowing = Math.floor(Math.random() * 3);
+    if (glowing == 0) {
+      setGlow({ red: "glow" });
+    } else if (glowing == 1) {
+      setGlow({ yellow: "glow" });
+    } else {
+      setGlow({ green: "glow" });
+    }
+  }, []);
+
+  return (
+    <div className="container-fluid">
+      <div className="stick"></div>
+      <div className="traffic-light">
+        <div
+          className={"red " + glow.red}
+          onClick={() => {
+            setGlow({ red: "glow" });
+          }}
+        ></div>
+        <div
+          className={"yellow " + glow.yellow}
+          onClick={() => {
+            setGlow({ yellow: "glow" });
+          }}
+        ></div>
+        <div
+          className={"green " + glow.green}
+          onClick={() => {
+            setGlow({ green: "glow" });
+          }}
+        ></div>
+      </div>
+      <button id="purple" className="btn">
+        Change color
+      </button>
+      <button className="btn">Add color</button>
+    </div>
+  );
 };
 
 export default Home;
